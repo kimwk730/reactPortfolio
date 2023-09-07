2f51536a1b9dd69068875c11a717b06bb6086b2b
kimwk730 <kimwk730@users.noreply.github.com>
2023-09-07 00:11:35 -1000

    Navbar and pages created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Projects from "./pages/Projects";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Navbar from "./components/Navbar";
 
 function App() {
-	return <div className="App">Hello World</div>;
+	return (
+		<div className="App">
+			<Router>
+				<Navbar />
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/projects" element={<Projects />} />
+					<Route path="/about" element={<About />} />
+					<Route path="/contact" element={<Contact />} />
+				</Routes>
+			</Router>
+		</div>
+	);
 }
 
 export default App;

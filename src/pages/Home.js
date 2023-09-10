076a2a6981684d114319acdd2ce5ed3f47dcac7b
kimwk730 <kimwk730@users.noreply.github.com>
2023-09-10 01:56:42 -1000

    hero content added

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "../styles/Home.css";
 
 function Home() {
-	return <div>Home</div>;
+	return (
+		<div className="home">
+			<div className="hero">
+				<h2>
+					<em>Hello, </em>I am
+				</h2>
+				<h1>WON KIM</h1>
+				<h3>Frontend developer based in Hawaii, US</h3>
+			</div>
+			<main>
+				<div>Projects</div>
+				<div>Skills</div>
+			</main>
+		</div>
+	);
 }
 
 export default Home;

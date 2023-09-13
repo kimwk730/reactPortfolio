fb9290c58bc46c56bc484dfe8ea09edb2033a87c
kimwk730 <kimwk730@users.noreply.github.com>
2023-09-12 23:06:50 -1000

    warning control

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,19 +5,19 @@ function ProjectItem({ image, name, skill, id, website, github }) {
 	return (
 		<div className="projectItem" id={id}>
 			<div className="projectPic">
-				<img src={image} className="projPic" alt="picpic" />
+				<img src={image} className="projPic" alt={name} />
 			</div>
 			<div className="info">
 				<h2>{name}</h2>
 				<p>{skill}</p>
 				<div className="links">
 					<button>
-						<a href={website} target="_blank">
+						<a href={website} target="_blank" rel="noreferrer">
 							Launch
 						</a>
 					</button>
-					<a href={github} target="_blank">
-						<img src={Github} className="githubLogo" />
+					<a href={github} target="_blank" rel="noreferrer">
+						<img src={Github} className="githubLogo" alt="github logo" />
 					</a>
 				</div>
 			</div>
